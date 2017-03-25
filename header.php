5677e575426e36c1956f2d500bca7ef7b9d82fe6
MoshCat <bas@middelham.nl>
2017-03-25 13:09:04 +0100

    Fixes

--- header.php
+++ header.php
@@ -8,34 +8,24 @@
 <link rel="preconnect" href="http://fonts.googleapis.com/" crossorigin>
 <script>
 	document.documentElement.className += ' wf-loading';
-	WebFontConfig = { 
-		google: {
-			families: [
-				'Open Sans:400,400i,600',
-				'Oswald:400'
-			]
-		},
-		timeout: 2000,
-	};
+	WebFontConfig = {google:{families:['Open Sans:400,400i,600','Oswald:400']},timeout: 2000};
 </script>
 <?php wp_head(); ?> 
 </head>
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'strt' ); ?></a>
-	<header id="masthead" itemscope itemtype="http://schema.org/Organization">
-		<div>
-			<div id="site-branding">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" itemprop="url">
-					<span itemprop="name"><?php bloginfo( 'name' ); ?></span>
-					<span itemprop="description"><?php echo get_bloginfo( 'description' ); ?></span>
-					<img width="190" height="48" id="site-logo" itemprop="logo" src="<?php echo get_template_directory_uri() ?>/img/starter-logo.png" alt="Logo <?php bloginfo( 'name' ); ?>" srcset="<?php echo get_template_directory_uri() ?>/img/starter-logo@2x.png 2x">
-				</a>
-			</div>
+	<div id="masthead" itemscope itemtype="http://schema.org/Organization">
+		<header>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" itemprop="url">
+				<span itemprop="name"><?php bloginfo( 'name' ); ?></span>
+				<span itemprop="description"><?php echo get_bloginfo( 'description' ); ?></span>
+				<img width="190" height="48" id="site-logo" itemprop="logo" src="<?php echo get_template_directory_uri() ?>/img/starter-logo.png" alt="Logo <?php bloginfo( 'name' ); ?>" srcset="<?php echo get_template_directory_uri() ?>/img/starter-logo@2x.png 2x">
+			</a>
 			<nav id="site-navigation">
 				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><span><?php esc_html_e( 'Menu', 'strt' ); ?></span></button><?php 
 				wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?> 
 			</nav>
-		</div>
-	</header>
+		</header>
+	</div>
 	<div id="content">
